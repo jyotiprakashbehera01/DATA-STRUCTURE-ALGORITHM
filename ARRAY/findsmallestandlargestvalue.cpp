f4adf598fdc62711095375995f071d98885a5afd
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-20 20:39:49 +0530

    findsmallestand largest array

--- ARRAY/findsmallestandlargestvalue.cpp
+++ ARRAY/findsmallestandlargestvalue.cpp
@@ -4,16 +4,20 @@ using namespace std;
 int main(){
 int nums[] = {5,34,76,85,56,-12};
 int size = 6;
-
+//smalest no find in array.
 int smallest = INT_MAX ;
+//Largesrt no fimd in array.
+int lardgest = INT_MIN ;
 
-for(int i=0;i<size;i++){
-    if(nums[i]<smallest){
-        smallest = nums[i];
+for(int i=0;i<size;i++){ //min,max
+   smallest = min(nums[i],smallest);
+   lardgest = max(nums[i],lardgest);
+
     }
-}
 
-cout << "smalest :"<<smallest<< endl;
+
+cout << "smalest = "<< smallest << endl;
+cout << "lardgest = " << lardgest << endl;
 return 0;
 
 }
