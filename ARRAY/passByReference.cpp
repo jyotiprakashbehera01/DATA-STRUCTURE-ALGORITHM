431503aab5ac8ed43131fb8fc55cf87e0f446b4a
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-20 21:03:23 +0530

    it has the by pass function

---
+++ ARRAY/passByReference.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void changeArr(int arr[], int size)
+{
+    cout << "in fumction\n";
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = 2 * arr[i];
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3};
+    changeArr(arr, 3);
+    cout << "in  main\n";
+    for (int i = 0; i < 3; i++)//2 4 6
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
