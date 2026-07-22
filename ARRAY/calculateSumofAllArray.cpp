7cbb35f35cc45c3e151c6979568d2cfbb6ed41e9
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-22 09:35:10 +0530

    sumOfArray

---
+++ ARRAY/calculateSumofAllArray.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sumofArray(int arr[], int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};   // Semicolon added
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Sum of array: " << sumofArray(arr, size);
+
+    return 0;
+}
