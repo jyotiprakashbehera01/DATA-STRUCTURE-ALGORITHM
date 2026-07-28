1e3fe01a89b9c1ec0fd4f56527498547f5468279
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-07-28 11:18:20 +0530

    pair sum

--- VECTORS/usingvectorpairsum.cpp
+++ VECTORS/usingvectorpairsum.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 //using function:
 vector<int> pairSum(vector<int>nums , int target){
-       vector<int>ans;
+       vector<int>ans;//when it has return
        int n = nums.size();
-       for(int i=0 ; i<n ; i++){
-           for(int j=i+1 ; j<n ; j++){
-               if(nums[i]+nums[j]==target){
+       //logic:
+       for(int i=0 ; i<n ; i++){//found pairs 
+           for(int j=i+1 ; j<n ; j++){//foud unique pair that has not back word
+               if(nums[i]+nums[j]==target){//codition check.
                 ans.push_back(i);
                 ans.push_back(j);
                 return ans;
@@ -22,8 +23,8 @@ int main(){
 vector<int> nums = {2,7,11,15};//given vector.
 int target = 9;//found the the target.
 
-vector<int> ans = pairSum( nums, target);
-cout << ans[0] << " ,  " << ans[1] << endl;
+vector<int> ans = pairSum( nums, target);//call back
+cout << ans[0] << " , " << ans[1] << endl;
 
 return 0;
 
