48c92a2786b4470220a2bbae86510b7cc45e6af0
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-13 13:30:59 +0530

    some upadate

--- ARRAY/findsmallestandlargestvalue.cpp
+++ ARRAY/findsmallestandlargestvalue.cpp
@@ -1,23 +1,22 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
-int main(){
-int nums[] = {5,34,76,85,56,-12};
-int size = 6;
-//smalest no find in array.
-int smallest = INT_MAX ;
-//Largesrt no fimd in array.
-int lardgest = INT_MIN ;
+int main()
+{
+    int nums[] = {5, 34, 76, 85, 56, -12};
+    int size = 6;
+    // smalest no find in array.
+    int smallest = INT_MAX;
+    // Largesrt no fimd in array.
+    int lardgest = INT_MIN;
 
-for(int i=0;i<size;i++){ //min,max
-   smallest = min(nums[i],smallest);
-   lardgest = max(nums[i],lardgest);
-
+    for (int i = 0; i < size; i++)
+    { // min,max
+        smallest = min(nums[i], smallest);
+        lardgest = max(nums[i], lardgest);
     }
 
-
-cout << "smalest = "<< smallest << endl;
-cout << "lardgest = " << lardgest << endl;
-return 0;
-
+    cout << "smalest = " << smallest << endl;
+    cout << "lardgest = " << lardgest << endl;
+    return 0;
 }
