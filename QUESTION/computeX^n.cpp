01d111918710de12c57ef5ad2be03de1c92400fe
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-18 08:41:07 +0530

    computeX^n

---
+++ QUESTION/computeX^n.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+
+        if (n == 0)
+            return 1.0;
+
+        if (x == 0)
+            return 0.0;
+
+        if (x == 1)
+            return 1.0;
+
+        if (x == -1)
+        {
+            if (n % 2 == 0)
+                return 1.0;
+            else
+                return -1.0;
+        }
+
+        long long binform = n;
+
+        if (binform < 0)
+        {
+            x = 1 / x;
+            binform = -binform;
+        }
+
+        double ans = 1.0;
+
+        while (binform > 0)
+        {
+            if (binform % 2 == 1)
+                ans *= x;
+
+            x *= x;
+            binform /= 2;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    double result = obj.myPow(2, 10);
+
+    cout << "Answer = " << result << endl;
+
+    return 0;
+}
